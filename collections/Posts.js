93d5fc8502cfc368b5dfb902d5aa326b244abf45
Rasmus Hjulskov Larsen <rhjulskov@gmail.com>
2015-01-28 23:22:31 +0100

    added scema2 + autoforms

--- collections/Posts.js
+++ collections/Posts.js
@@ -1,1 +1,25 @@
-Posts = new Meteor.Collection("posts");
+Posts = new Mongo.Collection("posts");
+
+var Schemas = {};
+
+Schemas.Posts = new SimpleSchema({
+    head: {
+        type: String,
+        label: "Title",
+        max: 200
+    },
+    name: {
+        type: String,
+        label: "Author"
+    },
+    mail: {
+        type: String,
+        label: "Email"
+    },
+    createDate: {
+      type: Date,
+      autoValue: function(){ return new Date() }
+    }
+});
+
+Posts.attachSchema(Schemas.Posts);
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,5 +1,5 @@
 if(Posts.find().count() === 0) {
-  Posts.insert({ head: "Hello World", name: "Peter Nielsen", mail: "peter@designmonkey", createDate: new Date() });
-  Posts.insert({ head: "Second Post", name: "Peter Nielsen", mail: "peter@designmonkey", createDate: new Date() });
-  Posts.insert({ head: "Bourbon + Neat has been added!", name: "Rasmus Hjulskov", mail: "rasmus@awesomenameinc.com", createDate: new Date() });
+  Posts.insert({ head: "Hello World", name: "Peter Nielsen", mail: "peter@designmonkey"});
+  Posts.insert({ head: "Second Post", name: "Peter Nielsen", mail: "peter@designmonkey" });
+  Posts.insert({ head: "Bourbon + Neat has been added!", name: "Rasmus Hjulskov", mail: "rasmus@awesomenameinc.com"});
 }
